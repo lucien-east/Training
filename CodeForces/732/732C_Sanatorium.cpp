ecee5c37a7d17e1539b7251a877d49cd3e055db3
lucien-east <lucien.cs03g@g2.nctu.edu.tw>
2016-11-13 00:52:38 -0800

    732C

---
+++ CodeForces/732/732C_Sanatorium.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <limits>
+#include <algorithm>
+#include <ios>
+#define endl ('\n')
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	// b=x[0], d=x[1], s=x[2]
+	long long x[3];
+	long long missed_meals;
+
+        while(std::cin >> x[0] >> x[1] >> x[2]) {
+		std::sort(x, x+3);
+		missed_meals = 2*(x[2]-1)-x[1]-x[0];
+		missed_meals = (x[1]!=x[2])?missed_meals:(std::max(0ll, x[2]-x[0]-1));
+		std::cout << missed_meals << endl;
+	}
+
+	return 0;
+}
