7bbc4d8404d1c7ac8deafb48ee2da7d2ae5fe475
lucien-east <lucien.cs03g@g2.nctu.edu.tw>
2017-03-21 11:24:40 -0700

    778E framework modified

--- CodeForces/778/778E_Selling_Numbers.cpp
+++ CodeForces/778/778E_Selling_Numbers.cpp
@@ -8,14 +8,15 @@ using namespace std;
 string B[N];
 unsigned int cost[10];
 bool carry[10][N];
-unsigned int digit_cnt[10];
+unsigned int previous_digit_cnt[10];
+unsigned int current_digit_cnt[10];
 unsigned int currnet_max_digit_cnt[10];
 
-long long price()
+long long price(const unsigned int *digit_cnt_table)
 {
 	long long sum = 0;
 	for(int i=0; i<10; ++i)
-		sum += (cost[i]*digit_cnt[i]);
+		sum += (cost[i]*digit_cnt_table[i]);
 
 	return sum;
 }
@@ -41,6 +42,7 @@ long long max_cost(const string &A, const unsigned int &n)
 	long long tmp_cost;
 	long long max = 0;
 	long long current_price[10];
+	long long previous_price[10];
 	unsigned int max_len;
 	string ans_A = A;
 
@@ -49,28 +51,49 @@ long long max_cost(const string &A, const unsigned int &n)
 		pos_B<max_len || pos_A>=0;
 		++pos_B, --pos_A)
 	{
-		/* pos_A: the least significant digit
-		 * pos_B: the most significant digit */
-		for(int i=0; i<n; ++i) {
-			/* rev_pos_B: Begin from the least significant digit of B[i] */
-			int rev_pos_B = (int) B[i].size() - pos_B - 1;
-			const char *tmp_B = B[i].c_str();
 
-			if(rev_pos_B>=0 && pos_A >=0) {
-				/* A[pos_A] + B[i][rev_pos_B] + carry */
+		for(int d=0; d<=9; ++d) {
 
-			}
-			else if(rev_pos_B<0 && pos_A>=0) {
-				/* A is longer than B[i]: A[pos_A] + carry */
+			for(int i=0; i<n; ++i) {
+				/***
+				 * pos_A: the least significant digit of A
+				 * pos_B: the most significant digit of B[i]
+				 * rev_B: the least significant digit of B[i]
+				 ***/
+				int rev_B = (int) B[i].size() - pos_B - 1;
+				const char *tmp_B = B[i].c_str();
+				int a, val;
 
-			}
-			else if(rev_pos_B>=0 && pos_A <0) {
-				/* B[i] is longer than A: B[i][rev_pos_B] + carry */
+				if(rev_B>=0 && pos_A >=0) {
+					/* A[pos_A] + B[i][rev_B] + carry */
+					a = ('?' == A[pos_A]) ? d : (A[pos_A]-'0');
+					val = a + (tmp_B[rev_B]-'0') + carry[d][i];
+				}
+				else if(rev_B<0 && pos_A>=0) {
+					/* A is longer than B[i]: A[pos_A] + carry */
+					a = ('?' == A[pos_A]) ? d : (A[pos_A]-'0');
+					val = a + carry[d][i];
+				}
+				else if(rev_B>=0 && pos_A <0) {
+					/* B[i] is longer than A: B[i][rev_B] + carry */
+					val = (tmp_B[rev_B]-'0') + carry[d][i];
+				}
 
-			}
+				carry[d][i] = (val > 9) ? true : false;
 
-		}
-	}
+				// TODO: update digit count
+
+			} /* B[0] to B[n] */
+
+
+			// TODO: Calculate the price
+
+			if(A[pos_A] != '?')	break;
+
+		} /* '?': 0 to 9 */
+
+
+	} /* the least to most significant digit */
 
 	return max;
 }
@@ -90,7 +113,7 @@ int main()
 	for(int i=0; i<10; ++i)
 		cin >> cost[i];
 
-	cout << "MAX = " << max_cost(A, n) << '\n';
+	cout << "\nMAX = " << max_cost(A, n) << '\n';
 
 	return 0;
 }
